0249129e2e4ed99149bb666dc5ddace8aac3f3c6
ChoiSoYoung0114 <2018112124@dongguk.edu>
2022-06-02 14:19:30 +0900

    0602 MODIFY main.py

--- app/main.py
+++ app/main.py
@@ -10,6 +10,7 @@ def result():
    if request.method == 'POST':
       result = dict()
       result['Name'] = request.form.get('Name')
+      result['Univ'] = request.form.get('Univ')
       result['Student Number'] = request.form.get('Student Number')
       result['Gender'] = request.form.get('Gender')
       result['Major'] = request.form.get('Major')
@@ -17,6 +18,7 @@ def result():
       result['languages'] =  ','.join(result['languages'])
 
       # 학번
+      # 대학
       # 성별
       # 학과
       # 프로그래밍 언어 -> hint) ','.join(list명)을 사용하면 list 안에 있는 항목들이 ','로 나누어져 출력됨.
